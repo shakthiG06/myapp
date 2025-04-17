71dea03ebc8cd59c619cdbdfe1a07cd8bb30e29f
shakthiG06 <shakthi3176@gmail.com>
2025-04-17 09:14:50 +0530

    populated data

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
-
-posts= [
-        {'id':1,'title':'Post 1','content':'content of post 1'},
-        {'id':2,'title':'Post 2','content':'content of post 2'},
-        {'id':3,'title':'Post 3','content':'content of post 3'},
-        {'id':4,'title':'Post 4','content':'content of post 4'},
-]
+from .models import Post
+#static demo data
+#posts= [
+#        {'id':1,'title':'Post 1','content':'content of post 1'},
+#        {'id':2,'title':'Post 2','content':'content of post 2'},
+#        {'id':3,'title':'Post 3','content':'content of post 3'},
+#        {'id':4,'title':'Post 4','content':'content of post 4'},
+#]
 def index(request):
     blog_title="Latest Posts"
+    posts=Post.objects.all()
     return render(request,'blog/index.html',{'blog_title':blog_title,'posts':posts})
 
 def detail(request,post_id):
